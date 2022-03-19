b968a92ce0fea42e4b09d973e48ab3ff5543fcfa
reneeb <info@perl-services.de>
2022-03-19 11:25:15 +0100

    changes for GPW2022

--- bin/bot.pl
+++ bin/bot.pl
@@ -1,86 +1,103 @@
 #!perl
-use strict; use warnings; use utf8;
-use Mojolicious::Matterbridge;
+
+use v5.24;
+
+use strict;
+use warnings;
+use utf8;
+
 use Etherpad;
-
-use YAML::Tiny;
-
+use Getopt::Long qw/:config auto_abbrev bundling/;
+use List::Util qw(first);
+use Mojolicious::Matterbridge;
+use Mojo::File qw(curfile);
 use Term::ANSIColor;
 use URI::Encode qw/uri_encode/;
+use YAML::Tiny;
 
-use Filter::signatures;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-use File::Basename;
-use Cwd 'abs_path';
-use Getopt::Long qw/:config auto_abbrev bundling/;
-my $config_path = abs_path(dirname(__FILE__)) . '/../cfg/bot.yml';
-GetOptions('config=s' => \$config_path);
+GetOptions(
+    'config=s' => \my $config_path,
+    'talks=s'  => \my $talks_path,
+);
 
-my $yaml = YAML::Tiny->read($config_path);
-my $config = $yaml->[0];
+$config_path //= curfile->dirname->child(qw/.. cfg bot.yml/);
+$talks_path  //= curfile->dirname->child(qw/.. cfg talks.yml/);
 
-my $bot_name = $config->{bot}->{name} || 'questions->etherpad';
-my $question_prefix = $config->{bot}->{question_prefix} || 'q:';
-my $etherpad_url = $config->{etherpad}->{url} || 'http://localhost:3712';
-my $etherpad_apikey = $config->{etherpad}->{apikey} 
-    || die 'Apikey for Etherpad must be specified in config.';
-my $etherpad_user = $config->{etherpad}->{user} || '';
-my $etherpad_pw = $config->{etherpad}->{password} || '';
-my $etp_title_prefix = $config->{etherpad}->{title_prefix} || '[Questions]';
+_run( $config_path, $talks_path );
+
+sub _run ( $config_path, $talks_path ) {
+    my $config = YAML::Tiny->read($config_path)->[0];
+    my $talks  = YAML::Tiny->read($talks_path)->[0];
+
+    my $etherpad = _connect_to_etherpad( $config );
+
+    my $client = Mojolicious::Matterbridge->new(
+        url => 'http://localhost:4242/api/',
+    );
+
+    my $talk = { id => -1 };
+
+    $client->on('message' => sub( $c, $message ) {
+        print colored(sprintf("<%s> %s", $message->username, $message->text), 'yellow'), "\n";
+
+        my $talk_timestamp = first { time > $_ } reverse sort keys $talks->%*;
+        my $current_talk   = $talks->{$talk_timestamp};
+
+        eval {
+            my @messages = _handle_message( $message );
+            $etherpad->append_text( $current_talk->{pad_id}, "$_\n" ) for @messages;
+        };
+        warn $@ if $@;
+
+        # if current_talk is different to talk, then send a message to matrix
+        # with the URL of the current pad TODO
+        if ( $current_talk->{id} != $talk->{id} ) {
+            $talk = $current_talk;
+            $client->send(sprintf "Fragen zum aktuellen Vortrag werden unter %s gesammelt", $talk->{url});
+        }
+    });
+
+    $client->connect();
+
+    Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+}
+
+sub _connect_to_etherpad ( $config ) {
+    my $etherpad_url     = $config->{etherpad}->{url} || 'http://localhost:3712';
+    my $etherpad_apikey  = $config->{etherpad}->{apikey}
+        || die 'Apikey for Etherpad must be specified in config.';
+
+    my $etherpad = Etherpad->new(
+        url    => $etherpad_url,
+        apikey => $etherpad_apikey,
+    );
+
+    if (!$etherpad->check_token()) {
+        die "API Token is not valid";
+    }
+
+    return $etherpad;
+}
 
 sub handle_message( $msg ) {
     my @questions;
+
+    my $question_prefix = $config->{bot}->{question_prefix} || 'q:';
+
     # Retrieve question
-    if( $msg->text =~ /^(?: $question_prefix\s | $question_prefix) (.*)$/xi ) {
+    if( $msg->text =~ m{^(?: $question_prefix\s | $question_prefix) (.*)$}xi ) {
         my $question = $1;
         # Append ? to question if not already present
-        $question .= '?' unless $question =~ /^.* \?$/x;
+        $question .= '?' unless $question =~ m{\?$}x;
+
         push @questions, sprintf "<%s> %s", $msg->username, $question;
     } else {
         #print sprintf "Ignoring '%s'\n", $msg->text;
     };
 
-    return @questions;
+    return $pad_id, @questions;
 }
 
-sub pad_url ( $base_url, $pad_id ) {
-    return uri_encode($base_url . '/p/' . $pad_id);
-}
-
-my $client = Mojolicious::Matterbridge->new(
-    url => 'http://localhost:4242/api/',
-);
-
-my $etherpad = Etherpad->new(
-    url         => "$etherpad_url",
-    apikey      => "$etherpad_apikey", 
-    user        => "$etherpad_user",
-    password    => "$etherpad_pw",
-);
-
-if (!$etherpad->check_token()) {
-    die "API Token is not valid";
-}
-
-my $pad_id = "$etp_title_prefix $ARGV[0]";
-
-eval { 
-    local $SIG{__WARN__} = sub {}; # Don't print warning if pad already exists
-    $etherpad->create_pad($pad_id, ""); 
-};
-
-print colored("Connected to Pad '$pad_id'", 'green'), "\n";
-print 'Link to Pad: ', colored(pad_url($etherpad_url, $pad_id), 'bright_blue'), "\n";
-
-$client->on('message' => sub( $c, $message ) {
-        print colored(sprintf("<%s> %s", $message->username, $message->text), 'yellow'), "\n";
-        eval {
-            $etherpad->append_text( $pad_id, "$_\n" ) for handle_message( $message);
-        };
-        warn $@ if $@;
-});
-$client->connect();
-
-Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
